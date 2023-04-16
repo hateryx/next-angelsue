08186a8bfbf459e014563d615c6990130ce1d182
Richmond S. Damag <112183555+hateryx@users.noreply.github.com>
2023-04-17 06:46:11 +0800

    Initialize

---
+++ src/components/landing/Hero.js
@@ -0,0 +1,5 @@
+function Hero() {
+  return <div className="min-h-screen">Hero</div>;
+}
+
+export default Hero;
---
+++ src/components/landing/NavBar.js
@@ -0,0 +1,9 @@
+function NavBar() {
+  return (
+    <div className="fixed top-0 inset-x-0 z-10 bg-white shadow p-10">
+      NavBar
+    </div>
+  );
+}
+
+export default NavBar;
